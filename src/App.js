b9300780fd7be8437fa44aea622e746d86e96731
barackmaund1 <pmaundu49@gmail.co>
2020-08-03 01:02:18 +0300

    fetch the data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cards from './components/Cards/Cards'
 import Charts from './components/Charts/Charts'
 import CountryPicker from './components/CountryPicker/CountryPicker'
 // import { Cards,Charts,CountryPicker} from './components'
+import { fetchData } from './api';
 
+class App extends React.Component{
+  state ={
+    data:{},
+  }
+  async componentDidMount(){
+    const data = await fetchData();
 
-function App() {
-  return (
-    <div className="container">
-      <Cards/>
-      <CountryPicker/>
-      <Charts/>
-    </div>
-  );
+    this.setState({ data:fetchData });
+  }
+ render(){
+  const { data }=this.state;
+   return(
+     
+     <div className='container'>
+     <Cards data={data}/>
+     <CountryPicker/>
+     <Charts/>
+     </div>
+   )
+ }
 }
 
 export default App;
+// function App() {
+ 
+//   return (
+//     <div className="container">
+//       <Cards/>
+//       <CountryPicker/>
+//       <Charts/>
+//     </div>
+//   );
+// }
+
+// export default App;
