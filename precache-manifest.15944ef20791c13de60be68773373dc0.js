25405ad15577746e4a95afd324229c8a1b89aeda
barackmaund1 <pmaundu49@gmail.co>
2021-01-05 13:33:53 +0300

    Updates

---
+++ precache-manifest.15944ef20791c13de60be68773373dc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926f3b9bed5e029764902295e8324e09",
+    "url": "/COVID-19-tracker-app/index.html"
+  },
+  {
+    "revision": "ebf44ce3997412033206",
+    "url": "/COVID-19-tracker-app/static/css/main.7a443eaf.chunk.css"
+  },
+  {
+    "revision": "91857c5540a7abe68dc0",
+    "url": "/COVID-19-tracker-app/static/js/2.142415d4.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/COVID-19-tracker-app/static/js/2.142415d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf44ce3997412033206",
+    "url": "/COVID-19-tracker-app/static/js/main.38e6a761.chunk.js"
+  },
+  {
+    "revision": "d9bda960a47e0c129dd1",
+    "url": "/COVID-19-tracker-app/static/js/runtime-main.2ebf9763.js"
+  },
+  {
+    "revision": "5cade2d8ba9b35a57c9fd7a825b7bff3",
+    "url": "/COVID-19-tracker-app/static/media/2021.5cade2d8.jpeg"
+  },
+  {
+    "revision": "2b455ccc0015fb29fed8cc12b3c976ac",
+    "url": "/COVID-19-tracker-app/static/media/IMG2.2b455ccc.jpg"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/COVID-19-tracker-app/static/media/covid19.6ce9074d.png"
+  }
+]);
